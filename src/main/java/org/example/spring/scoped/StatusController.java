4a4f1fd04f2b10072cae70a90d0217bfc996a309
Stephen Roebuck <sroebuck@veracode.com>
2026-06-11 09:16:47 -0400

    adding new status controller stub

---
+++ src/main/java/org/example/spring/scoped/StatusController.java
@@ -0,0 +1,32 @@
+package org.example.spring.scoped;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new_api")
+public class StatusController
+{
+    private final ScopedValue<StringBuilder> buffer_out = ScopedValue.newInstance();
+    private final ScopedValue<String> data = ScopedValue.newInstance();
+
+    @PostMapping(value = "/data_call", produces = MediaType.TEXT_HTML_VALUE)
+    public String data(@RequestBody String input) throws Exception
+    {
+        return ScopedValue.where(data, input)
+            .call(data::get);
+    }
+
+    @PostMapping(value = "/buffer_status", produces = MediaType.TEXT_HTML_VALUE)
+    public String status(@RequestBody String input) throws Exception
+    {
+        var buffer = new StringBuilder();
+        ScopedValue.where(buffer_out, buffer)
+            .run(() -> buffer_out.get().append(input));
+        return !buffer.toString().isEmpty()
+            ? "OK" : "Ignored";
+    }
+}
